3b181426344914df3872da14efb7e435db576f5f
Eaven Portillo <eaven@eavenportillo.com>
2010-01-21 14:21:18 -0800

    finished first cut of the User Model

--- config/routes.rb
+++ config/routes.rb
@@ -43,8 +43,9 @@ ActionController::Routing::Routes.draw do |map|
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.contact '/contact', :controller => 'pages', :action => 'contact'
-  
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.resources :users
 end
